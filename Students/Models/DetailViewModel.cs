90fc632fd96ace06bda159543b1e43b422bdf2e7
oguzkose <oguzkose2019@outlook.com>
2021-01-14 17:51:23 +0300

    added validation rules

--- Students/Models/DetailViewModel.cs
+++ Students/Models/DetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,18 @@ namespace Students.Models
 {
     public class DetailViewModel
     {
+        [Required]
+        [Range(1,int.MaxValue ,ErrorMessage = "Please enter a value greater than 0")]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
+        [Required]
         public string Classroom { get; set; }
+        [Required]
         public string Teacher { get; set; }
+        [Range(0,100 , ErrorMessage = "Grade Point Average must be between 0-100.")]
         public int GradeAvarage { get; set; }
         public bool HomeworkCompleted { get; set; }
     }
